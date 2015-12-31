b3c13976c17c6f6a6ba489de2ae8598c9a98d687
Brandon Parmenter <leftiness@gmail.com>
2015-12-31 18:03:16 -0500

    Clean fast script is easier to expand

--- bin/clean-fast.sh
+++ bin/clean-fast.sh
@@ -1,2 +1,14 @@
 #!/bin/bash
-find ./dist/* ! -iname 'vendor*' ! -iname 'font*' | xargs rm -rf
+IGNORE[0]='vendor*'
+IGNORE[1]='font*'
+IGNORE[2]='picnic*'
+IGNORE[3]='dialog*'
+CONCAT=''
+
+for each in "${IGNORE[@]}"
+do
+	CONCAT="$CONCAT ! -iname $each"
+done
+
+CMD="find ./dist/* $CONCAT | xargs rm -rf"
+eval $CMD
